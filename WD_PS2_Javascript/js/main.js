274fb68b78bdf2a204e5c0705eddc2d8964b89ee
shpp-nbezai <nikolaybezay@gmail.com>
2018-04-16 05:15:41 +0300

    Completed task 6/7/10

--- WD_PS2_Javascript/js/main.js
+++ WD_PS2_Javascript/js/main.js
@@ -1,6 +1,6 @@
 function getResultTask01(){
 	let sum = 0;
-  
+
 	for(let i = -1000; i <= 1000; i++){
 		sum = sum + i;
 	}
@@ -19,7 +19,7 @@ function getResultTask02(){
 }
 
 function getResultTask03(){
-  document.getElementById("content_task_03").innerHTML = '';
+	document.getElementById("content_task_03").innerText = ' ';
 	var sum = "*";
 
 	for(i = 0; i < 50; i++){
@@ -56,6 +56,21 @@ function secondsToTime(seconds){
   resultTime = resultHours + ':' + resultMinutes + ':' + resultSeconds;
 
   return resultTime;
+}
+
+function secondToDateAndTime(seconds){
+	const YEAR = 31104000, MONTH = 2592000, DAY = 86400;
+	let resultYears, resultMonths, resultDays, secondsRemainder, resultDate;
+
+	resultYears = Math.floor(seconds / YEAR);
+  resultMonths = Math.floor((seconds - (resultYears * YEAR)) / MONTH);
+  resultDays = Math.floor((seconds - (resultYears * YEAR) - (resultMonths * MONTH)) / DAY);
+	secondsRemainder = seconds - (resultYears * YEAR) - (resultMonths * MONTH) - (resultDays * DAY);
+
+	resultDate = resultYears + " years, " ;
+  resultDate += resultMonths + " month, ";
+  resultDate += resultDays + " days, and time: " + secondsToTime(secondsRemainder);
+	return resultDate;
 }
 
 function getResultTask04(){
@@ -98,10 +113,148 @@ function getResultTask05(){
 function getResultTask06(){
   let parseDateFirst = Date.parse(document.getElementById("dateCompareFirstInput").value);
   let parseDateSecond = Date.parse(document.getElementById("dateCompareSecondInput").value);
+	let resultTime, resultSeconds;
 
-  //if (parseDateFirst )
-  document.getElementById("resultTask06Label").innerText = new Date (parseDateFirst - parseDateSecond);  
+	if (isNaN(parseDateFirst) || isNaN(parseDateSecond)){
+		showErrorMessage("Error entering data. Please use the correct date format.", "compareDateErrorMsg");
+		return;
+	}
+	if (parseDateFirst > parseDateSecond){
+		showErrorMessage("You are trying to perform a non-correct comparison.", "compareDateErrorMsg");
+		return;
+	}
+	resultSeconds = (parseDateSecond - parseDateFirst) / 1000;
+	document.getElementById("resultTask06Label").innerText = secondToDateAndTime(resultSeconds);
 }
+
+function getResultTask07(){
+	let inputDate = Date.parse(document.getElementById("dateZodiacInput").value);
+	let birthday = new Date(inputDate);
+	const zod_signs = ["Capricorn" , "Aquarius", "Pisces", "Aries",
+	"Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio",
+	"Sagittarius"];
+
+	let day = birthday.getDate();
+	let month = birthday.getMonth();
+	let zodiacSign = "";
+
+  if (isNaN(inputDate)){
+    showErrorMessage("Error entering data. Please use the correct date format.", "zodiacErrorMsg");
+    return;
+  }
+
+	switch(month)
+	{
+		case 0: {//January
+				 if(day < 20)
+			 		zodiacSign = zod_signs[0];
+				 else
+			 		zodiacSign = zod_signs[1];
+			    }break;
+		case 1: {//February
+				 if(day < 19)
+			 		zodiacSign = zod_signs[1];
+				 else
+			 		zodiacSign = zod_signs[2];
+				}break;
+		case 2: {//March
+				 if(day < 21)
+				 	zodiacSign = zod_signs[2];
+				 else
+				 	zodiacSign = zod_signs[3];
+				}break;
+		case 3: {//April
+				 if(day < 20)
+			 		zodiacSign = zod_signs[3];
+				 else
+			 		zodiacSign = zod_signs[4];
+				}break;
+		case 4: {//May
+				 if(day < 21)
+			 		zodiacSign = zod_signs[4];
+				 else
+			 		zodiacSign = zod_signs[5];
+				}break;
+		case 5: {//June
+				 if(day < 21)
+			 		zodiacSign = zod_signs[5];
+				 else
+			 		zodiacSign = zod_signs[6];
+				}break;
+		case 6: {//July
+				 if(day < 23)
+			 		zodiacSign = zod_signs[6];
+				 else
+			 		zodiacSign = zod_signs[7];
+				}break;
+	 	case 7: {//August
+				 if(day < 23)
+			 		zodiacSign = zod_signs[7];
+				 else
+			 		zodiacSign = zod_signs[8];
+				}break;
+		case 8: {//September
+				 if(day < 23)
+			 		zodiacSign = zod_signs[8];
+				 else
+			 		zodiacSign = zod_signs[9];
+				}break;
+		case 9: {//October
+				 if(day < 23)
+			 		zodiacSign = zod_signs[9];
+				 else
+			 		zodiacSign = zod_signs[10];
+				}break;
+		case 10: {//November
+				 if(day < 22)
+			 		zodiacSign = zod_signs[10];
+				 else
+			 		zodiacSign = zod_signs[11];
+				}break;
+		case 11: {//December
+				 if(day < 22)
+			 		zodiacSign = zod_signs[11];
+				 else
+			 		zodiacSign = zod_signs[0];
+				}break;
+	 }
+	 	document.getElementById("resultTask07Label").innerText = zodiacSign;
+    document.getElementById("zodiacImg").src = "images/zodiac/" + zodiacSign + ".ico";
+}
+
+function getResultTask08(){
+  const shessFlag = {
+    flag = true;
+  };
+
+  shessFlag.flipFlag(){
+    flag = !flag;
+  }
+
+  shessFlag.flipFlag();
+  console.log(shessFlag.flag);
+}
+
+function sumDigitOnNumber(number){
+  let resultSum
+
+  if ( number == 0){
+    resultSum = 0;
+  } else {
+    resultSum = number % 10 + sumDigitOnNumber(Math.floor(number / 10));
+  }
+  return resultSum;  
+}
+
+function getResultTask10(){
+  let parseNumber = parseInt(document.getElementById("numberInput").value);
+
+  if ( !validateNumber(parseNumber, "calculateDigitErrorMsg")){
+    showErrorMessage("Please enter exactly the number!", "studentAgeErrorMsg");
+  }
+  document.getElementById("resultTask10Label").innerText = sumDigitOnNumber(parseNumber);
+}
+
 
 //getLinks.fiter(link => !link);
 // split
