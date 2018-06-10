9a1f72bbbd2eaece724ee52ca8b86d73ab13cbed
shpp-nbezai <nikolaybezay@gmail.com>
2018-06-11 02:42:23 +0300

    write createImage function

--- WD_PS3 jQuery/js/main.js
+++ WD_PS3 jQuery/js/main.js
@@ -15,11 +15,21 @@ const NAMES_ARRAY = [
   "Alexey"
 ];
 
+const ICO_FILES_SRC = {
+
+};
+
+
+function createImage( fileScr, DOMsrc) {
+  const image = document.createImage();
+  image.setAttribute( "src" , fileScr );
+  image.setAttribute( "class" , "button-ico" );
+  DOMsrc.appendChild( image );
+}
 
 function createDropdowndButton() {
   const RESULT_DIV =  document.getElementById( 'panel' );
   NAMES_ARRAY.map(item => {
-    console.log(item);
     let div = document.createElement("div");
     let button = document.createElement("button");
     button.setAttribute( 'class', 'dropdown-button' ); 
